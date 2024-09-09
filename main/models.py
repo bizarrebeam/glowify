e3e818ea95da6427e5235e44732e15cf4f6c7705
bizarrebeam <adelyaamanda@gmail.com>
2024-09-10 05:56:12 +0700

    edited models.py

--- main/models.py
+++ main/models.py
@@ -4,3 +4,9 @@ class Product(models.Model):
     name = models.CharField(max_length=255) 
     price = models.IntegerField()  
     description = models.TextField()  
+    volume = models.IntegerField()
+
+    
+    @property
+    def price_per_ml(self):
+        return self.price / self.volume if self.volume > 0 else 0
--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name' : 'sebuah barang',
-        'price': '10',
-        'description': 'oke bgt'
+        'name' : 'Adelya Amanda',
+        'npm': '2306165616',
+        'class': 'PBP-A'
     }
 
     return render(request, "main.html", context)
